ec129a60499c409dc95759c61c82b447b6c98b37
Nandini Kodipunzula <nandinik@FDLMC1821.local>
2018-07-13 10:47:44 +0530

    remove redundancies and refractor code

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,5 @@
 class TodosController < ApplicationController
-
+  before_action :set_todo, only: [:edit, :update, :show , :destroy]
   def new
    @todo =Todo.new
   end
@@ -14,24 +14,24 @@ class TodosController < ApplicationController
 
   end
   def edit
-     @todo = Todo.find(params[:id])
+     
   end
   def show 
-    @todo = Todo.find(params[:id])
+    
 
   end
   def index
     @todos=Todo.all
   end
   def destroy 
-    @todo = Todo.find(params[:id])
+   
     @todo.destroy
     flash[:notice] = "Todo is destroyed successfully"
     redirect_to todos_path
   end
 
   def update
-    @todo = Todo.find(params[:id])
+    
     if @todo.update(todo_params)
       flash[:notice] = "Todo is updated successfully" 
       redirect_to todo_path(@todo)
@@ -42,6 +42,10 @@ class TodosController < ApplicationController
   end
 
   private
+
+    def set_todo
+       @todo = Todo.find(params[:id])
+    end
 
   	def todo_params
   		params.require(:todo).permit(:name, :description)
